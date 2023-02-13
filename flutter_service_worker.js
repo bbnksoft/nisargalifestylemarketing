d6efba8b365790a21c6f3b2cd51570214bee842d
bbnk soft <bbnksoft.llp@gmail.com>
2023-02-13 23:06:49 +0530

    cart fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "2e4347828f2e12f4fb8c7cfc87a98d4e",
-"index.html": "e114e2fe4dea32f293472cba7fc9270f",
-"/": "e114e2fe4dea32f293472cba7fc9270f",
-"main.dart.js": "238a0444ee11a28d1c6e5f7f2ca48fac",
+"index.html": "7c7ee7ac248ab29f072666fe603dd28a",
+"/": "7c7ee7ac248ab29f072666fe603dd28a",
+"main.dart.js": "dbfd0539d0d183f420b427abb43e2262",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "90f7f04d5d122a5ade4147d5032b8f4f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
